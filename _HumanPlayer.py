dcd5246a5ffa162a549070704350867fdafe737d
Zhang Jun <zj1759653402@gmail.com>
2025-03-06 16:43:57 +0800

    Update new ver.

--- _HumanPlayer.py
+++ _HumanPlayer.py
@@ -1,14 +1,21 @@
 #####     Player     #####
+#####     Human Player     #####
 import pygame
-from _Player import *
+from _Player import PLAYER
 
 class HumanPl(PLAYER):
     """Human Player"""
     def __init__(self, side, name="机智如我"):
         super().__init__(side, name, mode="Human")
     
-    def ACT(self, events:list, step) -> tuple[int, int] | None:
-        """:param step: size of board grid"""
+    def ACT(self, events:list, step, *args, **kwargs) -> tuple[int, int] | None:
+        """Human Player's action
+        Args:
+            events: pygame events
+            step: size of board grid
+        Returns:
+            action: (y,x) or None
+        """
         for event in events:
             if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_focused():
                 x_pos, y_pos = pygame.mouse.get_pos()
